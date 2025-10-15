e85f2813140557b4e115f1401d44d77ed64173f8
Dakshsharma8266 <dakshsharma8266@gmail.com>
2025-10-15 11:20:02 +0530

    Create Task18.java

---
+++ Task18.java
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Task 18</title>
+</head>
+<body>
+  <aside>
+    <h3>Useful Links</h3>
+    <ul>
+      <li><a href="https://www.w3schools.com" target="_blank">W3Schools</a></li>
+      <li><a href="https://developer.mozilla.org" target="_blank">MDN Web Docs</a></li>
+    </ul>
+  </aside>
+</body>
+</html>
