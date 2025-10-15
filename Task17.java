77449fa3d8161b87bb5ffe05f89e9b3fceb1e1e2
Dakshsharma8266 <dakshsharma8266@gmail.com>
2025-10-15 11:19:42 +0530

    Create Task17.java

---
+++ Task17.java
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Task 17</title>
+</head>
+<body>
+  <main>
+    <article>
+      <h2>My First Blog</h2>
+      <p>This is my first blog post! I’m excited to share what I learn as I explore web development.</p>
+    </article>
+  </main>
+</body>
+</html>
