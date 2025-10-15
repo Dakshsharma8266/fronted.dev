734487eeb79661eaa4eb2610da8a88ca02132f06
Dakshsharma8266 <dakshsharma8266@gmail.com>
2025-10-15 11:13:59 +0530

    Create Task8.java

---
+++ Task8.java
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Task 8</title>
+</head>
+<body>
+  <p>This is a <mark>highlighted</mark> word in this paragraph. <small>Updated recently.</small></p>
+</body>
+</html>
