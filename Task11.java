7f66ef65e5427596b73b8bb69c6df3abd0aeb220
Dakshsharma8266 <dakshsharma8266@gmail.com>
2025-10-15 11:16:20 +0530

    Create Task11.java

---
+++ Task11.java
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Task 11</title>
+</head>
+<body>
+  <p>
+    I love <span style="color:red;">HTML</span>, 
+    <span style="color:green;">CSS</span>, 
+    and <span style="color:blue;">JavaScript</span>!
+  </p>
+</body>
+</html>
