732d09c12f8511a739fe5d289e63e4186dd21c78
Dakshsharma8266 <dakshsharma8266@gmail.com>
2025-10-15 11:20:30 +0530

    Create Task19.java

---
+++ Task19.java
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Task 19</title>
+</head>
+<body>
+  <footer>
+    <p>© 2025 Your Name. All rights reserved.</p>
+  </footer>
+</body>
+</html>
