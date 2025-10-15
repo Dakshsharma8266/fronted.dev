3018746fa3972062eeb0491e99eea8f771f8d933
Dakshsharma8266 <dakshsharma8266@gmail.com>
2025-10-15 11:16:45 +0530

    Create Task12.java

---
+++ Task12.java
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Task 12</title>
+</head>
+<body>
+  <p>
+    <strong>Learning HTML</strong> is <em>really fun</em>! 
+    You can <mark>highlight</mark> important text, 
+    write <small>tiny notes</small>, 
+    and break lines easily.<br>
+    <em>Keep practicing!</em>
+  </p>
+</body>
+</html>
