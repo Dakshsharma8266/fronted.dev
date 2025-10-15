78c28f8197a183c162891a3fea6348aab6ccfb8c
Dakshsharma8266 <dakshsharma8266@gmail.com>
2025-10-15 11:18:56 +0530

    Create Task15.java

---
+++ Task15.java
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Task 15</title>
+</head>
+<body>
+  <main>
+    <p>Hello! I’m a passionate web developer learning HTML and CSS. This page is all about my journey.</p>
+  </main>
+</body>
+</html>
