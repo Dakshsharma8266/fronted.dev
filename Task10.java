a741ace48988853529f09e2124d01a0eb07d1026
Dakshsharma8266 <dakshsharma8266@gmail.com>
2025-10-15 11:15:50 +0530

    Create Task10.java

---
+++ Task10.java
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Task 10</title>
+</head>
+<body>
+  <p>To print a message in JavaScript, type <code>console.log("Hello World");</code></p>
+</body>
+</html>
