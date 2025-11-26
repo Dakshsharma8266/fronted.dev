299e0c539bde9847014c0ba83e7ab3f481af82de
Dakshsharma8266 <dakshsharma8266@gmail.com>
2025-11-26 11:46:35 +0530

    Create greetUser Callback Flow.js

---
+++ greetUser Callback Flow.js
@@ -0,0 +1,13 @@
+// Q1.js - Callback Demonstration
+
+function greetUser(name, callback) {
+    console.log(`Hello ${name}`);
+    callback(); // execute callback after greeting
+}
+
+function showEndMessage() {
+    console.log("Welcome to the course!");
+}
+
+// Demonstration
+greetUser("Daksh", showEndMessage);
