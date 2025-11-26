9dc3af4bf7ba0d101226d6a96917ce681a2e901b
Dakshsharma8266 <dakshsharma8266@gmail.com>
2025-11-26 11:51:34 +0530

    Create makeMultiplier using Closure.js

---
+++ makeMultiplier using Closure.js
@@ -0,0 +1,9 @@
+// Q7.js
+function makeMultiplier(multiplier) {
+    return function (num) {
+        return num * multiplier;
+    };
+}
+
+const triple = makeMultiplier(3);
+console.log(triple(5));
